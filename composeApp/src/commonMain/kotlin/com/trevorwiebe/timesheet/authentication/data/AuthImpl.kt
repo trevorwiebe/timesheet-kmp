b1a99308d7d3c656edf439a33a5c1feb264893c8
trevor <thisistrevor4@gmail.com>
2025-02-14 12:36:47 -0600

    Refactor: Update getSignedInUser and other methods to use firebase.auth
    The `getSignedInUser`, `signIn`, and `resetPassword` methods in `AuthImpl` have been updated to use `firebase.auth` to obtain the `firebaseAuth` instance. This change removes the dependency on passing `FirebaseAuth` directly to the constructor and uses the `firebase` instance instead. This allows the `AuthImpl` class to retrieve the authenticated user.

--- composeApp/src/commonMain/kotlin/com/trevorwiebe/timesheet/authentication/data/AuthImpl.kt
+++ composeApp/src/commonMain/kotlin/com/trevorwiebe/timesheet/authentication/data/AuthImpl.kt
@@ -2,14 +2,16 @@ package com.trevorwiebe.timesheet.authentication.data
 
 import com.trevorwiebe.timesheet.authentication.domain.Authenticator
 import com.trevorwiebe.timesheet.core.domain.TSResult
-import dev.gitlive.firebase.auth.FirebaseAuth
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.auth
 
 class AuthImpl(
-    private val firebaseAuth: FirebaseAuth
+    private val firebase: Firebase
 ): Authenticator {
 
     override suspend fun getSignedInUser(): TSResult {
         try{
+            val firebaseAuth = firebase.auth
             val firebaseUser = firebaseAuth.currentUser
             return if(firebaseUser == null) {
                 TSResult(error = "User was null")
@@ -23,6 +25,7 @@ class AuthImpl(
 
     override suspend fun signIn(email: String, password: String): TSResult {
         try{
+            val firebaseAuth = firebase.auth
             val firebaseUser = firebaseAuth.signInWithEmailAndPassword(email, password).user
             return if(firebaseUser == null) {
                 TSResult(error = "User was null")
@@ -36,6 +39,7 @@ class AuthImpl(
 
     override suspend fun resetPassword(email: String): TSResult {
         try{
+            val firebaseAuth = firebase.auth
             firebaseAuth.sendPasswordResetEmail(email)
             return TSResult(data = "Reset password email sent")
         }catch (e: Exception) {
