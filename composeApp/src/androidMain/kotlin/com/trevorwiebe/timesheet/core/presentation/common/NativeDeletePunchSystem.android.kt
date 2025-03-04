9ccc772e0d69960233cd2ca66e4d9427d3b765c3
trevor <thisistrevor4@gmail.com>
2025-03-04 13:12:07 -0600

    update android delete punch dialog

--- composeApp/src/androidMain/kotlin/com/trevorwiebe/timesheet/core/presentation/common/NativeDeletePunchSystem.android.kt
+++ composeApp/src/androidMain/kotlin/com/trevorwiebe/timesheet/core/presentation/common/NativeDeletePunchSystem.android.kt
@@ -1,14 +1,30 @@
 package com.trevorwiebe.timesheet.core.presentation.common
 
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.trevorwiebe.timesheet.theme.tertiary
 
 @Composable
 actual fun NativeDeletePunchDialog(
@@ -25,20 +41,50 @@ actual fun NativeDeletePunchDialog(
     )
 
     if (showDialog) {
-        AlertDialog(
+        Dialog(
             onDismissRequest = { showDialog = false },
-            title = { Text("Are you sure?") },
-            text = { Text("Are you sure you want to delete this time? This will remove the punch-in and punch-out time.") },
-            confirmButton = {
-                Button(onClick = onDelete) {
-                    Text("Delete")
-                }
-            },
-            dismissButton = {
-                Button(onClick = { showDialog = false }) {
-                    Text("Cancel")
+        ) {
+            Card(
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        fontSize = 20.sp,
+                        text = "Are you sure?",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Are you sure you want to delete this time? This will remove the punch-in and punch-out time."
+                    )
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End,
+                    ) {
+                        TextButton(
+                            colors = ButtonDefaults.textButtonColors(contentColor = tertiary),
+                            onClick = { showDialog = false }
+                        ) {
+                            Text("Cancel")
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        TextButton(
+                            colors = ButtonDefaults.textButtonColors(contentColor = Color.Red),
+                            onClick = {
+                                showDialog = false
+                                onDelete()
+                            },
+                        ) {
+                            Text(
+                                text = "Delete"
+                            )
+                        }
+                    }
                 }
             }
-        )
+        }
     }
 }
