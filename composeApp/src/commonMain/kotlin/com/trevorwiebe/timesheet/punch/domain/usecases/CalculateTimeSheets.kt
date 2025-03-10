80a8df9bcd39d385e342b92686a3615946742d4e
trevor <thisistrevor4@gmail.com>
2025-03-10 11:08:04 -0500

    feat: Update CalculateTimeSheets use case
    - Update CalculateTimeSheets to use DateTimeUnit.DAY instead of DateTimePeriod.
    - Remove un-necessary imports.

--- composeApp/src/commonMain/kotlin/com/trevorwiebe/timesheet/punch/domain/usecases/CalculateTimeSheets.kt
+++ composeApp/src/commonMain/kotlin/com/trevorwiebe/timesheet/punch/domain/usecases/CalculateTimeSheets.kt
@@ -3,12 +3,10 @@ package com.trevorwiebe.timesheet.punch.domain.usecases
 import com.trevorwiebe.timesheet.core.domain.model.Organization
 import com.trevorwiebe.timesheet.core.domain.usecases.GetCurrentPayPeriodStartAndEnd
 import kotlinx.datetime.Clock
-import kotlinx.datetime.DateTimePeriod
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.plus
-import kotlinx.datetime.toLocalDateTime
 
 class CalculateTimeSheets(
     private val getCurrentPayPeriodStartAndEnd: GetCurrentPayPeriodStartAndEnd
@@ -19,15 +17,10 @@ class CalculateTimeSheets(
     ): List<Instant> {
 
         val currentDate = Clock.System.now()
-        val timeZone = TimeZone.UTC
-
         val payPeriodStartDate = getCurrentPayPeriodStartAndEnd(organization).first
 
-        val truncatedCurrentDate =
-            currentDate.toLocalDateTime(timeZone).date.atStartOfDayIn(timeZone)
-
-        return generateSequence(payPeriodStartDate) { it.plus(DateTimePeriod(days = 1), timeZone) }
-            .takeWhile { it <= truncatedCurrentDate }
+        return generateSequence(payPeriodStartDate) { it.plus(1, DateTimeUnit.DAY, TimeZone.UTC) }
+            .takeWhile { it <= currentDate }
             .toList()
     }
 }
