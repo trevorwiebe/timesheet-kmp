5da40070783f5401b764b8d94405ff290b216098
trevor <thisistrevor4@gmail.com>
2025-03-10 19:05:13 -0500

    feat: animate PunchItem visibility and scroll to last item
    - Add `AnimatedVisibility` to animate the appearance of `PunchItem` in the `LazyColumn`.
    - Add the ability to automatically scroll to the last item when the data is loaded.
    - Add a loading indicator when the content is not available.
    - Add a short delay to allow the data to load before showing it.

--- composeApp/src/commonMain/kotlin/com/trevorwiebe/timesheet/punch/presentation/PunchScreen.kt
+++ composeApp/src/commonMain/kotlin/com/trevorwiebe/timesheet/punch/presentation/PunchScreen.kt
@@ -1,5 +1,10 @@
 package com.trevorwiebe.timesheet.punch.presentation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,11 +14,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -24,6 +35,7 @@ import com.trevorwiebe.timesheet.core.presentation.common.TimeSheetButton
 import com.trevorwiebe.timesheet.punch.presentation.composables.PunchItem
 import com.trevorwiebe.timesheet.punch.presentation.uiUtils.UiPunch
 import com.trevorwiebe.timesheet.theme.tertiary
+import kotlinx.coroutines.delay
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -34,6 +46,25 @@ fun PunchScreen(
     val staticState by viewModel.staticPunchState.collectAsState()
     val dynamicState by viewModel.dynamicPunchState.collectAsState()
     val elementVisibilityState by viewModel.elementVisibilityState.collectAsState()
+
+    val listVisible = remember { mutableStateOf(false) }
+
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(staticState.timeSheetDateList, dynamicState.punches) {
+        if (staticState.timeSheetDateList.isNotEmpty()) {
+            listState.scrollToItem(staticState.timeSheetDateList.size - 1)
+        }
+    }
+
+    val contentUnavailable = staticState.timeSheetDateList.isEmpty()
+    LaunchedEffect(contentUnavailable) {
+        if (!contentUnavailable) {
+            delay(100)
+            listVisible.value = true
+        }
+    }
+
 
     Scaffold(
         topBar = {
@@ -50,38 +81,69 @@ fun PunchScreen(
             }
         }
     ) {
-        LazyColumn(
-            modifier = Modifier.padding(it).fillMaxSize()
-        ) {
-            items(staticState.timeSheetDateList) { todayDate ->
-                val punchList: List<UiPunch> = dynamicState.punches[todayDate] ?: emptyList()
-                PunchItem(
-                    date = todayDate,
-                    punches = punchList,
-                    onShowConfirmDelete = { uiPunch ->
-                        viewModel.onEvent(PunchEvents.OnShowConfirmDeletePunchesSheet(uiPunch))
-                    },
-                    onShowAddHours = { viewModel.onEvent(PunchEvents.OnShowAddHoursDialog(todayDate)) },
-                    onTimeSelected = { viewModel.onEvent(PunchEvents.OnUpdatePunch(it)) },
-                    onUpdateRate = {
-                        viewModel.onEvent(
-                            PunchEvents.OnUpdateRate(
-                                it.punchIn,
-                                it.punchOut
+
+        if (contentUnavailable) {
+            Box(
+                modifier = Modifier.padding(it).fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(color = tertiary)
+            }
+        } else {
+            AnimatedVisibility(
+                visible = listVisible.value,
+                enter = fadeIn(animationSpec = tween(500)),
+                exit = fadeOut()
+            ) {
+                LazyColumn(
+                    modifier = Modifier.padding(it).fillMaxSize(),
+                    state = listState
+                ) {
+                    items(staticState.timeSheetDateList) { todayDate ->
+                        val punchList: List<UiPunch> =
+                            dynamicState.punches[todayDate] ?: emptyList()
+                        PunchItem(
+                            date = todayDate,
+                            punches = punchList,
+                            onShowConfirmDelete = { uiPunch ->
+                                viewModel.onEvent(
+                                    PunchEvents.OnShowConfirmDeletePunchesSheet(
+                                        uiPunch
+                                    )
+                                )
+                            },
+                            onShowAddHours = {
+                                viewModel.onEvent(
+                                    PunchEvents.OnShowAddHoursDialog(
+                                        todayDate
+                                    )
+                                )
+                            },
+                            onTimeSelected = { viewModel.onEvent(PunchEvents.OnUpdatePunch(it)) },
+                            onUpdateRate = {
+                                viewModel.onEvent(
+                                    PunchEvents.OnUpdateRate(
+                                        it.punchIn,
+                                        it.punchOut
+                                    )
+                                )
+                            },
+                            rateList = staticState.rateList,
+                            hoursWorked = viewModel.getHoursWorkedForDay(
+                                punchList,
+                                staticState.rateList
                             )
                         )
-                    },
-                    rateList = staticState.rateList,
-                    hoursWorked = viewModel.getHoursWorkedForDay(punchList, staticState.rateList)
-                )
-            }
-            item {
-                AddPunch(
-                    loadingPunch = elementVisibilityState.punchLoading,
-                    onPunch = { viewModel.onEvent(PunchEvents.OnPunch) },
-                    onAddToPTO = {},
-                    buttonText = if (dynamicState.isClockedIn()) "Punch Out" else "Punch In"
-                )
+                    }
+                    item {
+                        AddPunch(
+                            loadingPunch = elementVisibilityState.punchLoading,
+                            onPunch = { viewModel.onEvent(PunchEvents.OnPunch) },
+                            onAddToPTO = {},
+                            buttonText = if (dynamicState.isClockedIn()) "Punch Out" else "Punch In"
+                        )
+                    }
+                }
             }
         }
     }
