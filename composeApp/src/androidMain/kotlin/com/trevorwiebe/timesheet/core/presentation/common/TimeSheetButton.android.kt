b29923d470c19c98dff25257f0d343866e308ebb
trevor <thisistrevor4@gmail.com>
2025-03-24 13:16:26 -0500

    update android button

--- composeApp/src/androidMain/kotlin/com/trevorwiebe/timesheet/core/presentation/common/TimeSheetButton.android.kt
+++ composeApp/src/androidMain/kotlin/com/trevorwiebe/timesheet/core/presentation/common/TimeSheetButton.android.kt
@@ -3,11 +3,11 @@ package com.trevorwiebe.timesheet.core.presentation.common
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,10 +24,10 @@ actual fun TimeSheetButton(
 ) {
     Button(
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = tertiary,
+            containerColor = tertiary,
         ),
         modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
+        shape = CircleShape,
         onClick = onClick,
         content = {
             Box(
